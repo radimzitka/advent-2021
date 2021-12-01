0e5b03bb3d904d43ce66e6a383384b7e910a70bf
radimzitka <radimzitka@radimzitka.cz>
2021-12-01 17:51:20 +0100

    First task

---
+++ 1.py
@@ -0,0 +1,23 @@
+with open('1.input') as file:
+  lines = file.readlines()
+  lines = [int(line.rstrip()) for line in lines]
+
+prev_sum = lines[0]
+counter = 0
+
+for number in lines:
+  if(number) > prev_sum:
+    counter += 1
+  prev_sum = number
+
+print("Task one:", counter)
+
+counter = 0
+prev_sum = lines[0] + lines[1] + lines[2]
+for i in range(1, len(lines) - 2):
+  curr_sum = lines[i] + lines[i + 1] + lines[i + 2]
+  if(curr_sum > prev_sum):
+    counter += 1
+  prev_sum = curr_sum
+
+print("Task two:", counter)
